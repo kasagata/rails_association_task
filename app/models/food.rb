f0813902a28a177755cd38afb66fe258fd078a0f
kasagata <k.sagata.1207@gmail.com>
2024-02-10 00:07:59 +0900

    仮7

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
     has_many :order_foods
-    has_and_belongs_to_many :shops
-    has_many :order, through: :order_foods
+    belongs_to :shop
+    has_many :orders, through: :order_foods
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,4 @@
 class Shop < ApplicationRecord
-    has_and_belongs_to_many :foods
+    has_many :foods
     has_many :addresses, as: :addressable
 end
