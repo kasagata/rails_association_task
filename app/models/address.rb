523d61a34660bb315d149efeca38d5d147971fbf
kasagata <k.sagata.1207@gmail.com>
2024-02-09 11:08:17 +0900

    課題提出

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+    belongs_to :customers
+    belongs_to :shops
+    has_many :orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+    belongs_to :customers
+    belongs_to :addresses
+    has_many :order_foods
 end
